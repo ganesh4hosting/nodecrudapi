792e3ac46d381ac70dc7fe8af3bdbd24251e5b89
Ganesh Sharma <sharma.ganesh413@gmail.com>
2018-07-08 20:11:15 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// hello how are you
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
